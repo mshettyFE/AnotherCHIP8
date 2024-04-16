bea8a725fbbb2466441af72994c86a8fa78223e1
Mihir Shetty <ms6556@columbia.edu>
2024-04-16 18:54:03 -0400

    Added better access control to Instruction struct

--- include/Instruction.h
+++ include/Instruction.h
@@ -9,12 +9,14 @@ private:
 public:
     Instruction(uint16_t instr);
     Instruction(uint8_t hhb, uint8_t lhb, uint8_t hlb, uint8_t llb);
-    uint8_t get_hhb() const{return hhb;};
-    uint8_t get_lhb() const{return lhb;};
-    uint8_t get_hlb() const{return hlb;};
-    uint8_t get_llb() const{return llb;};
-    uint16_t emit() const;
+    uint8_t get_hhb() const{return hhb;}
+    uint8_t get_lhb() const{return lhb;}
+    uint8_t get_hlb() const{return hlb;}
+    uint8_t get_llb() const{return llb;}
+    uint8_t get_lower_byte() const;
+    uint8_t get_upper_byte() const;
+    uint16_t get_mem_addr() const;
+    uint16_t get_machine_code() const;
 };
-
 
 #endif
--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -3,10 +3,10 @@
 Instruction::Instruction(uint16_t machine_code){
 // arguments from left to right are: highest 4 bits of high byte, lowest 4 bits of high byte, highest 4 bits of low byte, lowest 4 bits of low byte
 // the opposite of build instruction
-  this->hhb = uint8_t(machine_code >> 12 & 0x0F) ;
-  this->lhb = uint8_t(machine_code >> 8 & 0x0F);
-  this->hlb = uint8_t(machine_code >> 4 & 0x0F);
-  this->llb = uint8_t( machine_code & 0x0F);
+  this->hhb = static_cast<uint8_t>(machine_code >> 12 & 0x0F) ;
+  this->lhb = static_cast<uint8_t>(machine_code >> 8 & 0x0F);
+  this->hlb = static_cast<uint8_t>(machine_code >> 4 & 0x0F);
+  this->llb = static_cast<uint8_t>( machine_code & 0x0F);
 }
 
 Instruction::Instruction(uint8_t hhb, uint8_t lhb, uint8_t hlb, uint8_t llb){
@@ -16,8 +16,20 @@ Instruction::Instruction(uint8_t hhb, uint8_t lhb, uint8_t hlb, uint8_t llb){
   this->llb = llb & 0x0F;
 }
 
-uint16_t Instruction::emit() const{
+uint8_t Instruction::get_lower_byte() const{
+  return (hlb << 4) | hlb;
+}
+
+uint8_t Instruction::get_upper_byte() const{
+  return (hhb << 4) | lhb;
+}
+
+uint16_t Instruction::get_mem_addr() const{
+  return (static_cast<uint16_t>(get_lhb()) << 8) | (static_cast<uint16_t>(get_hlb()) << 4) | (static_cast<uint16_t>(get_llb()));
+}
+
+uint16_t Instruction::get_machine_code() const{
 // We first select the lowest 4 bits of each input, since you can't really pass in 4 bits as an argument without some confusing schnanigans
 // do the appropriate left shifting to each argument to get a properly formatted instruction
-  return ((uint16_t)this->hhb <<12)|((uint16_t)this->lhb << 8)|((uint16_t)this->hlb<< 4)|((uint16_t)this->llb);
+  return (static_cast<uint16_t>(this->hhb) <<12) | (static_cast<uint16_t>(this->lhb) << 8) | (static_cast<uint16_t>(this->hlb)<< 4) | (static_cast<uint16_t>(this->llb));
 }
