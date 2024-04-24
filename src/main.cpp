7f599f0e433a6876381e07038bbfa2aa9fcb5a5f
Mihir Shetty <ms6556@columbia.edu>
2024-04-24 19:17:39 -0400

    It technically runs now. But REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEALLY SLOWLY.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,15 @@
 int main(int argc, char *argv[]){
 
   if(argc != 2){
-    std::cout << "Need to provide a ROM file" << std::endl;
-    return 1;
+    CHIP8 interpreter(true);
+    interpreter.load("../roms/tetris.ch8");
+    interpreter.run_eternal();
+  }
+  else{
+    CHIP8 interpreter(true);
+    interpreter.load(argv[1]);
+    interpreter.run_eternal();
   }
 
-  CHIP8 interpreter(true);
-  interpreter.load(argv[1]);
-  interpreter.run_eternal();
   return 0;
 }
